b85a8c3b48b9ddca14c656aa5723f8e0f218a2c8
gorori <kousaku.maron@gmail.com>
2019-03-08 13:42:56 +0900

    style: add color palette file

---
+++ src/constants/colors.js
@@ -0,0 +1,19 @@
+const colors = {
+  priamry: {
+    main: '#087E8B'
+  },
+  secondary: {
+    main: '#FF5A5F'
+  },
+  tertiary: {
+    main: '#3C3C3C'
+  },
+  quaternary: {
+    main: '#F5F5F5'
+  },
+  quinary: {
+    main: '#C1839F'
+  },
+}
+
+export default colors
